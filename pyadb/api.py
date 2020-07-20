2ad294a7f0b14bb43bd54d45a7068cea1c612d8c
hawks.jamesf <hawks.jamesf@gmail.com>
2020-07-20 23:10:27 +0800

    add coro

---
+++ pyadb/api.py
@@ -0,0 +1,35 @@
+import asyncio
+import time
+
+
+async def say_after(delay, what):
+    await asyncio.sleep(delay)
+    print(what)
+
+
+async def main():
+    # 1
+    print(f"started at {time.strftime('%X')}")
+
+    await say_after(3, 'hello')
+    await say_after(1, 'world')
+
+    print(f"finished at {time.strftime('%X')}")
+
+    # 2
+    task1 = asyncio.create_task(
+        say_after(3, 'hello'))
+
+    task2 = asyncio.create_task(
+        say_after(1, 'world'))
+
+    print(f"started at {time.strftime('%X')}")
+
+    # Wait until both tasks are completed (should take
+    # around 2 seconds.)
+    await task1
+    await task2
+
+    print(f"finished at {time.strftime('%X')}")
+
+asyncio.run(main())
