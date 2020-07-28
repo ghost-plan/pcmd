c58a8c3240e27019178f7f694c491105ab9b2f66
电解质(Electrolyte) <Hawks.Jamesf@gmail.com>
2020-07-28 19:12:15 +0800

    Update log.py

--- pyadb/log.py
+++ pyadb/log.py
@@ -1,3 +1,7 @@
+from subprocess import Popen, PIPE, TimeoutExpired, run
+import os,signal
+from enum import Enum, unique
+import sys
 ANSI_RED = "\u001B[31m"
 ANSI_YELLOW = "\u001B[33m"
 ANSI_GREEN = "\u001B[32m"
@@ -7,11 +11,14 @@ ANSI_GREEN_BACKGROUND = "\u001B[42m"
 ANSI_YELLOW_BACKGROUND = "\u001B[43m"
 ANSI_BLUE_BACKGROUND = "\u001B[44m"
 ANSI_RESET = "\u001B[0m"
+
+
 def error(*message):
     s = ''
     for i in message:
         s = s+str(i)
     print(ANSI_RED+s+ANSI_RESET)
+
 
 def warn(*message):
     s = ''
@@ -19,11 +26,13 @@ def warn(*message):
         s = s+str(i)
     print(ANSI_YELLOW+s+ANSI_RESET)
 
+
 def info(*message):
     s = ''
     for i in message:
         s = s+str(i)
     print(ANSI_GREEN+s+ANSI_RESET)
+
 
 def debug(*message):
     s = ''
@@ -31,8 +40,85 @@ def debug(*message):
         s = s+str(i)
     print(ANSI_BLUE+s+ANSI_RESET)
 
+
 def verbose(*message):
     s = ''
     for i in message:
         s = s+str(i)
-    print(message)
+    print(s)
+
+# log.Format.BRIEF.value
+# log.Format.BRIEF.name
+@unique
+class Format(Enum):
+    NONE=0
+    BRIEF=1#<priority>/<tag>(<pid>): <message>
+    PROCESS=2#<priority>(<pid>) <message>
+    TAG =3#<priority>/<tag>: <message>
+    RAW=4#<message>
+    TIME=5#<datetime> <priority>/<tag>(<pid>): <message>
+    THREADTIME=6#<datetime> <pid> <tid> <priority> <tag>: <message>
+    LONG=7#[ <datetime> <pid>:<tid> <priority>/<tag> ] 
+          #<message>
+__pros=set(["V","D","I","W","E"])
+def __extra_priority(line,format):
+    if line is None or len(line)==0: return None
+    if format == Format.NONE:
+        ss = line.split()
+        return ss[4].strip() if len(ss) >5 and ss[4].strip() in __pros else None
+    elif format == Format.BRIEF:
+        ss=line.split("/")
+        return ss[0].strip() if len(ss) >0 and ss[0].strip() in __pros else None
+    elif format == Format.PROCESS:
+        ss=line.split("(")
+        return ss[0].strip() if len(ss) >0 and ss[0].strip() in __pros else None
+    elif format == Format.TAG:
+        ss=line.split("/")
+        return ss[0].strip() if len(ss) >0 and ss[0].strip() in __pros else None
+    elif format == Format.RAW:
+        return None
+    elif format == Format.TIME:
+        ss =line.split()
+        return ss[2].split("/")[0].strip() if len(ss)>3 and ss[2].split("/")[0].strip() in __pros else None
+    elif format == Format.THREADTIME:
+        ss = line.split()
+        return ss[4].strip() if len(ss) >5 and ss[4].strip() in __pros else None
+    elif format == Format.LONG:
+        return None
+def capture_log(serial_no,tags,format=Format.NONE):
+    if tags and len(tags)>0:
+        cmd = 'adb -s %s  shell logcat *:S' % (serial_no)
+        for t in tags:
+            cmd = cmd + ' %s:V ' % t.strip()
+    else:
+        cmd = 'adb -s %s  shell logcat' % (serial_no)
+    if format != Format.NONE:
+        cmd = cmd + ' -v %s' % format.name.lower()
+    with Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True,
+               preexec_fn=os.setsid, encoding='utf-8') as pipe:
+        try:
+            # for line in iter(lambda: pipe.stdout.readline(), ''):
+            while True:
+                line = pipe.stdout.readline()
+                prior = __extra_priority(line,format)
+                if "V" == prior:
+                    verbose(line)
+                    pass
+                elif "D" == prior:
+                    debug(line)
+                    pass
+                elif "I" == prior:
+                    info(line)
+                    pass
+                elif "W" == prior:
+                    warn(line)
+                    pass
+                elif "E" == prior:
+                    error(line)
+                    pass
+                else:
+                    print(line)
+        except KeyboardInterrupt as e:
+            os.killpg(pipe.pid, signal.SIGINT)
+        except TimeoutExpired as e:
+            os.killpg(pipe.pid, signal.SIGINT)
