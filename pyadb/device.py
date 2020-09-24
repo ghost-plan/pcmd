a86e744443624ed104ac81f7f98d543cd0a41a31
Hawks Jamesf <hawks.jamesf@gmail.com>
2020-09-24 16:34:00 +0800

    [feat] add sample

--- pyadb/device.py
+++ pyadb/device.py
@@ -221,16 +221,18 @@ def get_imeis(serial_no):
         return imei
 
 
-table = {'38e7a0': 'vivo', '389a78': 'HONOR'}
+table = {'38e7a0': 'vivo/V1911A/PD1911', '2479f3': 'OPPO/PCAM10/PCAM10', '002e25': 'OPPO/PCGM00/PCGM00',
+         '389a78': 'HONOR/YAL-AL00/YAL-AL00', '189b3b': 'HUAWEI/POT-AL00a/POT-AL00a', '08f56e': 'HUAWEI/MAR-AL00/MAR-AL00',
+         '303a51': 'xiaomi/Redmi Note 7/lavender', '00ad16': 'xiaomi/MI 5X/tiffany'}
 
 
 @check_device
 def parse_mac(serial_no, mac_bytes: bytes = None, mac_str: str = 0):
     '''
     MAC地址共48位（6个字节），以十六进制表示。第1Bit为广播地址(0)/群播地址(1)，第2Bit为广域地址(0)/区域地址(1)。前3~24位由IEEE决定如何分配给每一家制造商，且不重复，后24位由实际生产该网络设备的厂商自行指定且不重复。
-    vivo/V1911A:f8:e7:a0:8c:ed:a5
+    vivo/V1911A/PD1911:f8:e7:a0:8c:ed:a5
     vivo厂商：3-24位固定值：0x38E7A0
-    HONOR/YAL-AL00:f8:9a:78:50:2f:7e
+    HONOR/YAL-AL00/AL-AL00:f8:9a:78:50:2f:7e
     HONOR厂商：3-24位固定值:0x389a78
     '''
     mac = None
@@ -247,7 +249,7 @@ def parse_mac(serial_no, mac_bytes: bytes = None, mac_str: str = 0):
     second_bit = (m3[0] >> 6) & 0b01
     m3[0] &= 0b00111111
     dom = m3.hex()
-    dom_name = table[dom]
+    dom_name = table.get(dom)
     m3_6 = mac[3:6]
 
     return first_bit, second_bit, (dom_name, dom), m3_6.hex()
@@ -371,6 +373,13 @@ def main():
             [0xf8, 0x9a, 0x78, 0x50, 0x2f, 0x7e])))
 
         print(parse_mac(d, mac_str='f8:e7:a0:8c:ed:a5'))
+        print(parse_mac(d, mac_str='d8:9b:3b:6d:71:a5'))
+        print(parse_mac(d, mac_str='d8:9b:3b:6d:72:c4'))
+        print(parse_mac(d, mac_str='88:f5:6e:06:6d:7a'))
+        print(parse_mac(d, mac_str='70:3a:51:88:f3:91'))
+        print(parse_mac(d, mac_str='80:ad:16:5d:58:38'))
+        print(parse_mac(d, mac_str='24:79:f3:a5:ef:31'))
+        print(parse_mac(d, mac_str='c0:2e:25:da:a4:8f'))
         # print(get_ip(d))
         # print(get_ip(d))
         # print(get_ip(d))
