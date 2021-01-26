f7a836ed3618c70fb1b7b97b5f6a161fb597a2d6
Hawks Jamesf <hawks.jamesf@gmail.com>
2021-01-26 13:57:51 +0800

    update adb_cmd fiel

--- pyadb/adb_cmd.py
+++ pyadb/adb_cmd.py
@@ -1,35 +1,56 @@
-from subprocess import TimeoutExpired, PIPE, DEVNULL, Popen, STDOUT, PIPE, run, TimeoutExpired
-import functools
-import os
-import sys
-import re
 import time
-def _cmd(serial_no, cmd_list):
-    completedProcess = run(cmd_list,shell=True, stdout=PIPE, stderr=PIPE)
+import re
+import sys
+import os
+import functools
+from subprocess import TimeoutExpired, PIPE, DEVNULL, Popen, STDOUT, PIPE, run, TimeoutExpired
+
+
+def __cmd_list(cmd_list):
+    completedProcess = run(cmd_list, shell=True, stdout=PIPE, stderr=PIPE)
     returncode = completedProcess.returncode
     if returncode == 0:
         o = completedProcess.stdout.decode('utf-8').strip()
         return (o.split('\n') if o else o, returncode)
     else:
         return (completedProcess.stderr.decode('utf-8').strip(), returncode)
+
+
 def adb_shell_cmd(serial_no, cmd_list):
-    return _cmd(serial_no, 'adb -s %s shell %s' % (serial_no, cmd_list))
-
-def adb_push_cmd(serial_no, cmd_list):
-    return _cmd(serial_no, 'adb -s %s push %s' % (serial_no, cmd_list))
+    return __cmd_list('adb -s %s shell %s' % (serial_no, cmd_list))
 
 
-def get_devices():
-    with os.popen("adb devices") as p:
-        ret = p.readlines()
-        if ret is None or len(ret) == 0:
-            return []
-        ds = []
-        for line in ret[1:]:
-            if (re.match(".*device$", line)):
-                ds.append(line.split("\t")[0])
-        return ds
-if __name__ =='__main__':
-    for d in get_devices():
-        ret = adb_shell_cmd(d, 'settings get secure android_id')
-        print(ret)
+def adb_push_cmd(serial_no, src, dest):
+    return __cmd_list('adb -s %s push %s %s' % (serial_no, src, dest))
+
+
+def adb_pull_cmd(serial_no, src, dest):
+    return __cmd_list('adb -s %s pull %s %s' % (serial_no, src, dest))
+
+
+def adb_input_cmd(serial_no, source='', subcmd='', *args):
+    '''
+    The sources are:
+        mouse
+        keyboard
+        joystick
+        touchnavigation
+        touchpad
+        trackball
+        stylus
+        dpad
+        gesture
+        touchscreen
+        gamepad
+    '''
+    if len(subcmd) == 0:
+        raise BaseException('subcmd must be not empty')
+    s = ''
+    for i in args:
+        s = s+' '+str(i)
+    cmd = 'adb -s %s shell input %s %s %s' % (serial_no, source, subcmd, s)
+    __cmd_list(cmd)
+
+
+if __name__ == '__main__':
+    pass
